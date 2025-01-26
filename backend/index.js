6ddaf57b7a320adebc968b4ce7e2bececac99cb6
virtualabishek <717abishekneupane@gmail.com>
2025-01-26 14:06:32 +0545

    backend complete

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import formRouter from "./routes/formRouter.js";
+import pool from "./models/index.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Backend is working.");
+});
+
+app.use("/form-submit", formRouter);
+
+app.listen(8000, () => {
+  console.log("Server has started.");
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Unable to connect to the database:", err.message);
+      process.exit(1);
+    } else {
+      console.log(
+        "Connection to the database has been established successfully."
+      );
+      connection.release();
+    }
+  });
+});
